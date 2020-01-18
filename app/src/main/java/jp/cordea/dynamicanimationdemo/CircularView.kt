0a895dd17d93c19baa262f1d65086bf2e35c2031
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-18 22:42:26 +0900

    start spring animation

--- app/src/main/java/jp/cordea/dynamicanimationdemo/CircularView.kt
+++ app/src/main/java/jp/cordea/dynamicanimationdemo/CircularView.kt
@@ -6,6 +6,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.dynamicanimation.animation.SpringAnimation
 
 class CircularView @JvmOverloads constructor(
     context: Context,
@@ -16,6 +17,17 @@ class CircularView @JvmOverloads constructor(
         style = Paint.Style.FILL
         isAntiAlias = true
         color = ContextCompat.getColor(context, R.color.colorAccent)
+    }
+
+    fun startAnimation(
+        stiffness: Float,
+        dampingRatio: Float
+    ) {
+        scaleX = 1.5f
+        SpringAnimation(this, SpringAnimation.SCALE_X, 1f).apply {
+            spring.stiffness = stiffness
+            spring.dampingRatio = dampingRatio
+        }.start()
     }
 
     override fun onDraw(canvas: Canvas?) {
--- app/src/main/java/jp/cordea/dynamicanimationdemo/MainListItem.kt
+++ app/src/main/java/jp/cordea/dynamicanimationdemo/MainListItem.kt
@@ -1,19 +1,32 @@
 package jp.cordea.dynamicanimationdemo
 
 import com.xwray.groupie.databinding.BindableItem
+import com.xwray.groupie.databinding.GroupieViewHolder
 import jp.cordea.dynamicanimationdemo.databinding.MainListItemBinding
 
 class MainListItemViewModel(
-    private val stiffness: Float,
-    private val dampingRatio: Float
+    val stiffness: Float,
+    val dampingRatio: Float
 )
 
 class MainListItem(
     private val viewModel: MainListItemViewModel
 ) : BindableItem<MainListItemBinding>() {
+    private var binding: MainListItemBinding? = null
+
     override fun getLayout(): Int = R.layout.main_list_item
 
     override fun bind(viewBinding: MainListItemBinding, position: Int) {
+        binding = viewBinding
         viewBinding.viewModel = viewModel
+    }
+
+    override fun unbind(viewHolder: GroupieViewHolder<MainListItemBinding>) {
+        super.unbind(viewHolder)
+        binding = null
+    }
+
+    fun startAnimation() {
+        binding?.circularView?.startAnimation(viewModel.stiffness, viewModel.dampingRatio)
     }
 }
