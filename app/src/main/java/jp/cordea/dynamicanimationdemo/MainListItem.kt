1bb5467611aa135ad0c3eee3d2815857dd46243d
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-18 22:24:48 +0900

    define viewmodel

--- app/src/main/java/jp/cordea/dynamicanimationdemo/MainListItem.kt
+++ app/src/main/java/jp/cordea/dynamicanimationdemo/MainListItem.kt
@@ -3,9 +3,17 @@ package jp.cordea.dynamicanimationdemo
 import com.xwray.groupie.databinding.BindableItem
 import jp.cordea.dynamicanimationdemo.databinding.MainListItemBinding
 
-class MainListItem : BindableItem<MainListItemBinding>() {
+class MainListItemViewModel(
+    private val stiffness: Float,
+    private val dampingRatio: Float
+)
+
+class MainListItem(
+    private val viewModel: MainListItemViewModel
+) : BindableItem<MainListItemBinding>() {
     override fun getLayout(): Int = R.layout.main_list_item
 
     override fun bind(viewBinding: MainListItemBinding, position: Int) {
+        viewBinding.viewModel = viewModel
     }
 }
